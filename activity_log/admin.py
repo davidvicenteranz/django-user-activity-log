687b12f8153ecbbdc5abb78412f62ac82331aa94
david <dvicente74@gmail.com>
2017-04-08 22:14:27 +0100

    Prettyfy Json field using Pygments

--- activity_log/admin.py
+++ activity_log/admin.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
+import json
+from pygments import highlight
+from pygments.lexers.data import JsonLexer
+from pygments.formatters import HtmlFormatter
 
 from django.contrib import admin
 from . import models
@@ -12,7 +16,29 @@ class LogAdmin(admin.ModelAdmin):
     date_hierarchy = 'datetime'
     list_filter = ('request_method', 'response_code')
     search_fields = ('user', 'request_url')
-    readonly_fields = ('user_id','user','request_url','request_method','response_code','datetime','extra_data','ip_address')
+    readonly_fields = ('user_id','user','request_url','request_method','response_code','datetime','data_prettified','ip_address')
 
+    def data_prettified(self, instance):
+        """Function to display pretty version of our data"""
+
+        # Convert the data to sorted, indented JSON
+        response = json.dumps(instance.extra_data, sort_keys=True, indent=2)
+
+        # Truncate the data. Alter as needed
+        response = response[:5000]
+
+        # Get the Pygments formatter
+        formatter = HtmlFormatter(style='colorful')
+
+        # Highlight the data
+        response = highlight(response, JsonLexer(), formatter)
+
+        # Get the stylesheet
+        style = "<style>" + formatter.get_style_defs() + "</style><br>"
+
+        # Safe the output
+        return mark_safe(style + response)
+
+    data_prettified.short_description = 'extra data'
 
 admin.site.register(models.ActivityLog, LogAdmin)
--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
 
     install_requires=[
         'jsonfield',
+        'Pygments'
     ],
 
     classifiers=(
