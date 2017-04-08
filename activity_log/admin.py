1629cdeb5b1fac75dbc8b01a97f865d19d2a278e
david <dvicente74@gmail.com>
2017-04-08 22:23:30 +0100

    exclude extra_data field and added lazy translate for prettify field

--- activity_log/admin.py
+++ activity_log/admin.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
-import json
+from django.utils.translation import ugettext_lazy as _
 from django.utils.safestring import mark_safe
 from pygments import highlight
 from pygments.lexers.data import JsonLexer
 from pygments.formatters import HtmlFormatter
-
 from django.contrib import admin
 from . import models
-
+import json
 
 class LogAdmin(admin.ModelAdmin):
     list_display = ('user', 'user_id', 'request_method', 'request_url',
@@ -18,6 +17,7 @@ class LogAdmin(admin.ModelAdmin):
     list_filter = ('request_method', 'response_code')
     search_fields = ('user', 'request_url')
     readonly_fields = ('user_id','user','request_url','request_method','response_code','datetime','data_prettified','ip_address')
+    exclude = ('extra_data',)
 
     def data_prettified(self, instance):
         """Function to display pretty version of our data"""
@@ -40,6 +40,6 @@ class LogAdmin(admin.ModelAdmin):
         # Safe the output
         return mark_safe(style + response)
 
-    data_prettified.short_description = 'extra data'
+    data_prettified.short_description = _('extra data')
 
 admin.site.register(models.ActivityLog, LogAdmin)
