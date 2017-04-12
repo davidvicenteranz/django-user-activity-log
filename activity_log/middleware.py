819316d8691de0c3f8ade027b65cdaff6bfe4e24
david <dvicente74@gmail.com>
2017-04-12 15:37:23 +0100

    compatible with Django 1.10

--- activity_log/middleware.py
+++ activity_log/middleware.py
@@ -23,6 +23,8 @@ def get_extra_data(request, response, body):
     return _load(conf.GET_EXTRA_DATA)(request, response, body)
 
 class ActivityLogMiddleware(MiddlewareMixin):
+    def __init__(self, get_response=None):
+        self.get_response = get_response
 
     def process_request(self, request):
         request.saved_body = request.body
@@ -30,10 +32,12 @@ class ActivityLogMiddleware(MiddlewareMixin):
             getattr(request.user, 'update_last_activity', lambda: 1)()
 
     def process_response(self, request, response):
+
         try:
             self._write_log(request, response, getattr(request, 'saved_body', ''))
         except DisallowedHost:
             return HttpResponseForbidden()
+
         return response
 
     def _write_log(self, request, response, body):
@@ -42,7 +46,6 @@ class ActivityLogMiddleware(MiddlewareMixin):
             request.method not in conf.METHODS,
             any(url in request.path for url in conf.EXCLUDE_URLS)
         ]
-
         if conf.STATUSES:
             miss_log.append(response.status_code not in conf.STATUSES)
 
@@ -58,7 +61,7 @@ class ActivityLogMiddleware(MiddlewareMixin):
             user, user_id = 'anon_{}'.format(request.session.session_key), 0
         else:
             return
-
+        #print "end"
         ActivityLog.objects.create(
             user_id=user_id,
             user=user,
@@ -68,6 +71,23 @@ class ActivityLogMiddleware(MiddlewareMixin):
             ip_address=get_ip_address(request),
             extra_data=get_extra_data(request, response, body)
         )
+        return
+
+    # def process_request(self, request):
+    #     request.saved_body = request.body
+    #     print dir(request)
+    #     if conf.LAST_ACTIVITY and request.user.is_authenticated():
+    #         getattr(request.user, 'update_last_activity', lambda: 1)()
+    #
+    # def process_response(self, request, response):
+    #     try:
+    #         self._write_log(request, response, getattr(request, 'saved_body', ''))
+    #         pass
+    #     except DisallowedHost:
+    #         return HttpResponseForbidden()
+    #     return response
+    #
+
     #
     # def __call__(self, request):
     #     response = self.get_response(request)
